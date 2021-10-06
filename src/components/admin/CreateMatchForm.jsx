25380b08a4a906a2064ec62f8b660bd9bacbbfbb
Nicolas Martinez <martinez.nicolas0510@gmail.com>
2021-10-06 20:04:10 -0300

    :lipstick: cambio select por autocomplete en formulario de crear partido

--- src/components/admin/CreateMatchForm.jsx
+++ src/components/admin/CreateMatchForm.jsx
@@ -1,12 +1,13 @@
-import MenuItem from '@mui/material/MenuItem';
 import AddIcon from '@mui/icons-material/Add';
-import { Box, Button, Grid, Paper } from '@mui/material';
+import { Box, Button, Grid, Paper,InputLabel } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import GridItem from './GridItem';
 import BeginningTypography from "../beginning/BeginningTypography";
 import matchService from '../../services/MatchService';
 import { useEffect, useState } from 'react';
 import BackdropInherit from '../feedback/Backdrop';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 function CreateMatchForm({ onSubmit }) {
     const { register, handleSubmit } = useForm({
@@ -22,97 +23,94 @@ function CreateMatchForm({ onSubmit }) {
 
     useEffect(() => {
         matchService.teams()
-            .then((response) => setTeams([undefined].concat(response.data)))
+            .then((response) => { setTeams([].concat(response.data)); console.log(response.data) })
             .catch((error) => console.log(error.response));
     }, []);
 
     const renderTeams = () => {
-        return teams?.map((team, i) => <MenuItem key={i} value={team?.name}>{team?.name || 'Selecciona un equipo'}</MenuItem>)
+        return teams.map((team) => team.name)
     }
-    
+
     return (
         <>
-        { teams !== null ?
-            <>
-            <BeginningTypography text="Agregar partido" />
-            <Paper
-                component="form"
-                noValidate
-                onSubmit={handleSubmit(onSubmit)}
-                sx={{
-                    backgroundColor: '#d7dbdd',
-                    p: 2,
-                    borderRadius: 2,
-                    mb: 1
-                }}
-                elevation={6}
-                >
-                <Grid container spacing={1} sx={{ display: 'flex' }}>
-                    <GridItem
-                        register={register}
-                        name="home"
-                        isSelect={true}
-                        id="home-id"
-                        label="Local"
-                        content={renderTeams()}
-                        xs={12}
-                    />
-                    <GridItem
-                        register={register}
-                        name="away"
-                        isSelect={true}
-                        id="away-id"
-                        label="Visitante"
-                        content={renderTeams()}
-                        xs={12}
-                    />
-                    <GridItem
-                        register={register}
-                        name="date"
-                        type="date"
-                        isSelect={false}
-                        id="date-id"
-                        label="Fecha de partido"
-                        xs={12}
-                    />
-    
-                    <GridItem
-                        register={register}
-                        name="time"
-                        type="time"
-                        isSelect={false}
-                        id="time-id"
-                        label="Hora de partido"
-                        xs={12}
-                    />
-                    <GridItem
-                        register={register}
-                        name="price"
-                        type="number"
-                        isSelect={false}
-                        id="price-id"
-                        label="Precio"
-                        xs={12}
-                    />
-    
-                </Grid>
-                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2, maxWidth: '50%' }}
+            {teams !== null ?
+                <>
+                    <BeginningTypography text="Agregar partido" />
+                    <Paper
+                        component="form"
+                        noValidate
+                        onSubmit={handleSubmit(onSubmit)}
+                        sx={{
+                            backgroundColor: '#d7dbdd',
+                            p: 2,
+                            borderRadius: 2,
+                            mb: 1
+                        }}
+                        elevation={6}
                     >
-                        <AddIcon sx={{ mr: 1 }} /> Crear partido
-                    </Button>
-                </Box>
-            </Paper>
-            </>
-            :
-            <BackdropInherit open={true}/>
-        }
+                        <Grid container spacing={1} sx={{ display: 'flex' }}>
+                            <Grid item xs={12} >
+                            <InputLabel sx={{ paddingBottom: 1}}>Local</InputLabel>
+                                <Autocomplete
+                                    freeSolo
+                                    fullWidth
+                                    options={renderTeams()}
+                                    renderInput={(params) => <TextField {...params} label="Selecciona un equipo" />}
+                                />
+                            </Grid>
+                            <Grid item xs={12} >
+                            <InputLabel sx={{ paddingBottom: 1}}>Visitante</InputLabel>
+                                <Autocomplete
+                                    freeSolo
+                                    fullWidth
+                                    options={renderTeams()}
+                                    renderInput={(params) => <TextField {...params} label="Selecciona un equipo" />}
+                                />
+                            </Grid>
+                            <GridItem
+                                register={register}
+                                name="date"
+                                type="date"
+                                id="date-id"
+                                label="Fecha de partido"
+                                xs={12}
+                            />
+
+                            <GridItem
+                                register={register}
+                                name="time"
+                                type="time"
+                                id="time-id"
+                                label="Hora de partido"
+                                xs={12}
+                            />
+                            <GridItem
+                                register={register}
+                                name="price"
+                                type="number"
+                                id="price-id"
+                                label="Precio"
+                                xs={12}
+                            />
+
+                        </Grid>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: 2 }}>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2, maxWidth: '50%' }}
+                            >
+                                <AddIcon sx={{ mr: 1 }} /> Crear partido
+                            </Button>
+                        </Box>
+                    </Paper>
+                </>
+                :
+                <BackdropInherit open={true} />
+            }
         </>
-        
+
     )
 }
 
