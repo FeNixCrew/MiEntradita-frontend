aabfa95b2f633e4bb5adcc4203bf5579ccfa5f84
Nicolas Martinez <martinez.nicolas0510@gmail.com>
2021-10-14 23:00:05 -0300

    :lipstick: Modifico interfaz y agrego boton de logout en SelectMatch component

--- src/components/scanner/AvailableMatch.jsx
+++ src/components/scanner/AvailableMatch.jsx
@@ -7,24 +7,25 @@ import Typography from '@mui/material/Typography';
 
 import { formatDateAndTime } from '../../helpers/usedFunctions';
 
-export default function AvailableMatch({match, setMatch}) {
+
+export default function AvailableMatch({ match, setMatch }) {
     return (
-        <div >
-        <Grid item md={12} style={{marginTop:'2vh'}}>
-            <Card style={{ padding: 1, backgroundColor: '#373737'}}>
-                <CardContent style={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2" sx={{color: "#2e86c1"}}>
-                        {`${match.home} vs ${match.away}`}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="h2" sx={{color: "#2e86c1"}}>
-                      {formatDateAndTime(match.matchStartTime)}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small" style={{color: '#2e86c1'}} onClick={() => setMatch(match)}>Seleccionar</Button>
-                </CardActions>
-            </Card>
-        </Grid>
+        <div>
+            <Grid item md={12} sx={{ marginTop: '5vh' }}>
+                <Card style={{ padding: 1, backgroundColor: '#373737' }}>
+                    <CardContent style={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2" sx={{ color: "#2e86c1" }}>
+                            {`${match.home} vs ${match.away}`}
+                        </Typography>
+                        <Typography gutterBottom variant="h5" component="h2" sx={{ color: "#2e86c1" }}>
+                            {formatDateAndTime(match.matchStartTime)}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" style={{ color: '#2e86c1' }} onClick={() => setMatch(match)}>Seleccionar</Button>
+                    </CardActions>
+                </Card>
+            </Grid>
         </div>
     );
 }
--- src/components/scanner/SelectMatch.jsx
+++ src/components/scanner/SelectMatch.jsx
@@ -6,13 +6,14 @@ import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 import BackdropInherit from '../feedback/Backdrop';
 import SnackBar from '../feedback/SnackBar';
+import BeginningTypography from '../BeginningTypography';
 
 import { useToggle } from '../../helpers/hooks/useToggle';
 import matchService from '../../services/MatchService';
 import AvailableMatch from './AvailableMatch';
 import { exit } from '../../helpers/usedFunctions';
 
-export default function SelectMatch({setMatch}) {
+export default function SelectMatch({ setMatch }) {
   const [isLoading, setIsLoading] = useState(false);
   const [matchs, setMatchs] = useState(null);
   const [isOpenSnack, closeSnackBar, openSnackBar] = useToggle();
@@ -30,50 +31,63 @@ export default function SelectMatch({setMatch}) {
       .catch(_ => {
         setScanMessage('No se pudo obtener los partidos de hoy. Por favor, refresque la pagina.')
         setResultState('error');
-        openSnackBar(); 
+        openSnackBar();
         setIsLoading(false);
       })
   }, [openSnackBar]);
 
-  const renderAvailableGames = () => matchs.map(match => <AvailableMatch match={match} setMatch={setMatch}/>);
+  const renderAvailableGames = () => matchs.map(match => <AvailableMatch match={match} setMatch={setMatch} />);
 
   return (
-    <div style={{
-      display: 'grid',
-      'grid-template-columns': 'repeat(auto-fit, auto-fit)',
-      'grid-gap': '1vw',
-      'justify-content': 'center',
-      'height': '100vh'
-    }}>
-    { !matchs ?
-      <BackdropInherit open={isLoading} />
-        : matchs.length === 0 ?
-          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            <h2 style={{color: '#EEEEEE'}}>No hay partidos disponibles, inténtelo más tarde</h2>
-            <Button
-            style={{
-              color: '#2e86c1'
-            }}
-            onClick={() => exit(history)}
-            sx={{
-              mt: '1vh'
-            }}>
-            <ExitToAppIcon />
-            </Button>
-          </div>
-          :
-          <div>
-            <h1 style={{color: '#EEEEEE'}}>De qué partido desea validar entradas?</h1>
-            {renderAvailableGames()}
-          </div>
-      }
-    <SnackBar
-      openSnackBar={isOpenSnack}
-      severityState={resultState}
-      message={scanMessage}
-      closeSnackBar={closeSnackBar}
-      position={{vertical: 'bottom', horizontal: 'right'}}
-    /> 
-    </div>
+    <>
+      <Button
+        style={{
+          color: '#2e86c1'
+        }}
+        onClick={() => exit(history)}
+        sx={{
+          mt: '1vh'
+        }}
+      >
+        <ExitToAppIcon />
+      </Button>
+      <div style={{
+        display: 'grid',
+        'grid-template-columns': 'repeat(auto-fit, auto-fit)',
+        'grid-gap': '1vw',
+        'justify-content': 'center',
+        'height': '100vh'
+      }}>
+        {!matchs ?
+          <BackdropInherit open={isLoading} />
+          : matchs.length === 0 ?
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <h2 style={{ color: '#EEEEEE' }}>No hay partidos disponibles, inténtelo más tarde</h2>
+              <Button
+                style={{
+                  color: '#2e86c1'
+                }}
+                onClick={() => exit(history)}
+                sx={{
+                  mt: '1vh'
+                }}>
+                <ExitToAppIcon />
+              </Button>
+            </div>
+            :
+            <div>
+              <BeginningTypography text="De qué partido desea validar entradas?" variant="h4" sx={{ color: '#EEEEEE' }} />
+              {renderAvailableGames()}
+            </div>
+        }
+        <SnackBar
+          openSnackBar={isOpenSnack}
+          severityState={resultState}
+          message={scanMessage}
+          closeSnackBar={closeSnackBar}
+          position={{ vertical: 'bottom', horizontal: 'right' }}
+        />
+      </div>
+    </>
   )
 }
