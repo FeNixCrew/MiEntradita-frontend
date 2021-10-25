3d2bb9483e84894ccf6aeecad7294380e2396de5
Nicolas Martinez <martinez.nicolas0510@gmail.com>
2021-10-25 14:44:22 -0300

    :lipstick: Modifico interfaz de seleccionar partido en scanner

--- src/components/scanner/AvailableMatch.jsx
+++ src/components/scanner/AvailableMatch.jsx
@@ -12,17 +12,17 @@ export default function AvailableMatch({ match, setMatch }) {
     return (
         <div>
             <Grid item md={12} sx={{ marginTop: '5vh' }}>
-                <Card style={{ padding: 1, backgroundColor: '#373737' }}>
+                <Card style={{ padding: 1, backgroundColor: '#eecf92' }}>
                     <CardContent style={{ flexGrow: 1 }}>
-                        <Typography gutterBottom variant="h5" component="h2" sx={{ color: "#2e86c1" }}>
+                        <Typography gutterBottom variant="h5" component="h2" sx={{ color: 'black' }}>
                             {`${match.home} vs ${match.away}`}
                         </Typography>
-                        <Typography gutterBottom variant="h5" component="h2" sx={{ color: "#2e86c1" }}>
+                        <Typography gutterBottom variant="h5" component="h2" sx={{ color: 'black' }}>
                             {formatDateAndTime(match.matchStartTime)}
                         </Typography>
                     </CardContent>
                     <CardActions>
-                        <Button size="small" style={{ color: '#2e86c1' }} onClick={() => setMatch(match)}>Seleccionar</Button>
+                        <Button size="small" style={{ color: 'black' }} onClick={() => setMatch(match)}>Seleccionar</Button>
                     </CardActions>
                 </Card>
             </Grid>
