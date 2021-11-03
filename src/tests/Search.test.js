f033bb64a27ea8b662f527bd3ab6ce210afb5d2e
Nicolas Martinez <martinez.nicolas0510@gmail.com>
2021-11-03 12:35:00 -0300

    :bug: arreglo error en los tests que se arrastro con las abstracciones

--- src/tests/Search.test.js
+++ src/tests/Search.test.js
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Search from '../pages/Search';
-import SearchResults from '../components/search/SearchResults';
+import RenderMatchesComponent from "../components/RenderMatchesComponent";
+import { ComponentToRenderWhenReturn } from '../pages/Search'
 
 describe('Buscador', () => {
 
@@ -11,24 +12,24 @@ describe('Buscador', () => {
     });
 
     test('Cuando la busqueda no encuentra resultados, se muesta un titulo indicandolo', () => {
-      render(<SearchResults results={[]}/>)
+      render(<RenderMatchesComponent matches={[]} ComponentToRenderWhenReturn={ComponentToRenderWhenReturn} />)
       expect(screen.getByText(/no se han encontrado partidos/i)).toBeInTheDocument();
     });
 
     test('Cuando la busqueda tiene resultados, se muestra un titulo indicandolo', () => {
-      render(<SearchResults results={[
+      render(<RenderMatchesComponent matches={[
         {id: 1, home: 'a', away: 'b', matchStartTime: "2021-10-26T10:32:47.013646"},
         {id: 2, home: 'd', away: 'e', matchStartTime: "2021-10-26T10:32:47.013646"},
-      ]}/>)
+      ]} ComponentToRenderWhenReturn={ComponentToRenderWhenReturn} />)
 
       expect(screen.getByText(/resultados:/i)).toBeInTheDocument();
     });
 
     test('Cuando la busqueda tiene resultados, cada elemento del mismo esta representado', () => {
-      const { getAllByTestId } = render(<SearchResults results={[
+      const { getAllByTestId } = render(<RenderMatchesComponent matches={[
                                   {id: 1, home: 'a', away: 'b', matchStartTime: "2021-10-26T10:32:47.013646"},
                                   {id: 2, home: 'd', away: 'e', matchStartTime: "2021-10-26T10:32:47.013646"},
-                                ]}/>)
+                                ]} ComponentToRenderWhenReturn={ComponentToRenderWhenReturn} />)
 
       expect(getAllByTestId('search-item').length).toEqual(2);
     });
