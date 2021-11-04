3541b588f73b8841d0d0db56e1678947f6260b7f
Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
2021-11-03 22:37:55 -0300

    :recycle: agrego componente que borre por error
    Signed-off-by: Fede Sandoval <federico.sandoval@alu.unq.edu.ar>

---
+++ src/components/RenderMatchesComponent.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import spectatorService from '../services/SpectatorService'
+import { isUser } from "../helpers/usedFunctions";
+import MatchCard from "./MatchCard";
+
+function RenderMatchesComponent({ ComponentToRenderWhenReturn, matches }) {
+    const [teamId, setTeamId] = useState(null);
+
+    useEffect(() => {
+        if (isUser()) {
+            const savedTeamId = parseInt(localStorage.favouriteTeamId) || null;
+            setTeamId(savedTeamId);
+        }
+    }, []);
+
+    const markAsFavourite = (newTeamId) => {
+        if (newTeamId === teamId) {
+            spectatorService.markAsFavourite(teamId);
+            localStorage.favouriteTeamId = null;
+            setTeamId(null);
+        } else {
+            localStorage.favouriteTeamId = newTeamId;
+            setTeamId(newTeamId);
+            spectatorService.markAsFavourite(newTeamId);
+        }
+    }
+
+    const haveFavouriteTeam = () => {
+        return teamId !== null;
+    }
+
+    const renderMatches = () => {
+        return matches.map((match, i) =>
+            <MatchCard
+                key={i}
+                match={match}
+                teamId={teamId}
+                markAsFavourite={markAsFavourite}
+                haveFavouriteTeam={haveFavouriteTeam}
+            />
+        )
+    }
+
+    return (
+        <ComponentToRenderWhenReturn render={renderMatches} matches={matches} />
+    );
+};
+
+export default RenderMatchesComponent;
