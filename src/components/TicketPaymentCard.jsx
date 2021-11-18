b8b0ad4ef278bb9c1b1a727041dd55497c6d1a48
Nicolas Martinez <martinez.nicolas0510@gmail.com>
2021-11-18 12:19:58 -0300

    :lipstick: mejorando look and feel de pagina para pagar entradas
    Co-Authored-By: Fede Sandoval <federico.sandoval@alu.unq.edu.ar>
    Signed-off-by: Nicolas Martinez <martinez.nicolas0510@gmail.com>

--- src/components/TicketPaymentCard.jsx
+++ src/components/TicketPaymentCard.jsx
@@ -1,21 +1,83 @@
-function TicketPaymentCard({ticket}) {
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { makeStyles } from '@material-ui/core';
+import { CardHeader } from '@mui/material';
+import { Box } from '@mui/system';
+import { formatDateAndTime } from '../helpers/usedFunctions';
+
+const useStyle = makeStyles((_) => ({
+    root: {
+        marginTop: '1vh',
+        padding: '2vh',
+    },
+    cardComp: {
+        padding: '1vh',
+        width: 'auto',
+        height: 'auto',
+        maxHeight: '20vw',
+        backgroundColor: '#ecf0f1',
+        maxWidth: '40vw',
+        minHeight: '15vw',
+        minWidth: '15vw'
+    },
+    data: {
+        fontFamily: 'Quicksand',
+        fontStyle: 'italic',
+        paddingBottom: '1vh',
+        fontSize: 15,
+    },
+    priceContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'baseline',
+        marginBottom: '1vh',
+    }
+}))
+
+function TicketPaymentCard({ ticket }) {
+    const classes = useStyle();
+
+    const horarioFormateado = formatDateAndTime(ticket.matchStartTime)
+
     const payTicket = () => {
         window.open(ticket.link, '_self');
     }
 
     return (
         <div>
-            {
-            ticket ?
-                <div>
-                    <h1>{ticket.home} vs {ticket.away} </h1>
-                    <h3> Valor: ${ticket.price}</h3>
-                    <button onClick={payTicket}> PAGAR AHORA</button>
-                </div> :
-             <h1>Cargando...</h1>
+            {ticket
+                ? <Grid item className={classes.root}>
+                    <Card className={classes.cardComp}>
+                        <CardHeader
+                            title={<div style={{ fontSize: 21, textAlign: 'center' }}>{ticket.home} vs {ticket.away}</div>}
+                            subheader={horarioFormateado}
+                            titleTypographyProps={{ align: 'center' }}
+                            subheaderTypographyProps={{
+                                align: 'center',
+                            }}
+                        />
+                        <CardContent>
+                            <Box className={classes.priceContainer}>
+                                <Typography component="h2" variant="h3" color="text.primary">
+                                    ${ticket.price}
+                                </Typography>
+                            </Box>
+                        </CardContent>
+                        <CardActions>
+                            <Button fullWidth variant="contained" onClick={payTicket}>
+                                Pagar
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                : <Typography>Cargando</Typography>
             }
         </div>
-    );
+    )
 }
 
 export default TicketPaymentCard;
--- src/pages/PendingPayments.jsx
+++ src/pages/PendingPayments.jsx
@@ -6,10 +6,43 @@ import { useSnackbar } from "../helpers/hooks/useSnackbar";
 import TicketPaymentCard from "../components/TicketPaymentCard";
 import SnackBar from "../components/feedback/SnackBar";
 import BurgerMenu from "../components/navigation/BurgerMenu";
+import { Container, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+import PayIcon from '../assets/pay_icon.png'
+import { Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+
+const useStyle = makeStyles((_) => ({
+    banner: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginBottom: '7vh',
+        backgroundColor: '#2e86c1',
+        padding: '3vh',
+        marginLeft: '10vh',
+        marginRight: '10vh',
+        marginTop: '3vh'
+    },
+    bannerLogo: {
+        width: '8vw',
+        height: '8vw'
+    },
+    container: {
+        marginTop: '3vh',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column'
+    }
+}))
+
 
 function PendingPaymentsPage() {
     const [setError, _, isOpenSnack, closeSnackBar, severity, message] = useSnackbar();
     const [pendingPayments, setPendingPayments] = useState(null);
+    const classes = useStyle();
 
     useEffect(() => {
         spectatorService.pendingPayments()
@@ -22,32 +55,38 @@ function PendingPaymentsPage() {
     }, [setError]);
 
     const showPendingTicketsPayment = () => {
-        return pendingPayments.map(ticket => <TicketPaymentCard key={ticket.id} ticket={ticket}/>);
+        return pendingPayments.map(ticket => <TicketPaymentCard key={ticket.id} ticket={ticket} />);
     }
 
     return (
         <div>
             <SnackBar
-            openSnackBar={isOpenSnack}
-            severityState={severity}
-            message={message}
-            closeSnackBar={closeSnackBar}
-            position={{ vertical: 'bottom', horizontal: 'left' }}
+                openSnackBar={isOpenSnack}
+                severityState={severity}
+                message={message}
+                closeSnackBar={closeSnackBar}
+                position={{ vertical: 'bottom', horizontal: 'left' }}
             />
             {
-            pendingPayments ? 
-                <div>
-                    {showPendingTicketsPayment()}
-                </div>
-                :
-                <h1> Cargando... </h1>
+                pendingPayments
+                    ? <div>
+                        <Box component={Paper} className={classes.banner}>
+                            <img className={classes.bannerLogo} src={PayIcon} alt="pay icon" />
+                        </Box>
+                        <Container className={classes.container} >
+                            <Grid container>
+                                {showPendingTicketsPayment()}
+                            </Grid>
+                        </Container>
+                    </div>
+                    : <Typography>Cargando...</Typography>
             }
         </div>
     )
 }
 
 function PendingPayments() {
-    return <BurgerMenu children={<PendingPaymentsPage/>} />
+    return <BurgerMenu children={<PendingPaymentsPage />} />
 }
 
 export default PendingPayments;
