dad8d4aaffa4cfbe6b538153738df3d4701428a1
Nicolas Martinez <martinez.nicolas0510@gmail.com>
2021-11-18 20:29:20 -0300

    :recycle: Extraigo estilos a coustom hook
    Signed-off-by: Nicolas Martinez <martinez.nicolas0510@gmail.com>

--- src/pages/SuccessTicketPayment.jsx
+++ src/pages/SuccessTicketPayment.jsx
@@ -6,10 +6,43 @@ import Check from '../assets/check-mark.png'
 import { Typography } from "@material-ui/core";
 import CircularProgress from '@mui/material/CircularProgress';
 import { useState } from "react";
+import { makeStyles } from "@material-ui/styles";
+
+
+const useStyle = makeStyles((_) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    checkContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    check: {
+        width: '10vw',
+        height: '10vw',
+        marginTop: '13vh'
+    },
+    message1: {
+        fontStyle: 'italic',
+        marginTop: '4vh',
+        fontFamily: 'Quicksand'
+    },
+    message2: {
+        fontStyle: 'italic',
+        marginBottom: '3vh',
+        fontSize: 17,
+        marginTop: '1vh',
+        fontFamily: 'Quicksand'
+    }
+}))
 
 function Success({ ticketId, payment_id }) {
     const [progress, setProgress] = useState(0);
     const history = useHistory()
+    const classes = useStyle();
 
     useEffect(() => {
         spectatorService.savePayment(ticketId, payment_id);
@@ -32,21 +65,17 @@ function Success({ ticketId, payment_id }) {
         <div>
             {
                 ticketId && payment_id
-                    ? <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                        <Box sx={{ width: '90vw', height: '70vh', marginTop: '5vh'}}>
-                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                                <img src={Check} alt="check mark" style={{ width: '10vw', height: '10vw', marginTop: '13vh' }} />
-                                <Typography component='h2' variant='h4' style={{ fontStyle: 'italic', marginTop: '4vh' }}>¡Compra exitosa!</Typography>
-                                <Typography
-                            style={{ fontStyle: 'italic', marginBottom: '3vh', fontSize: 17, marginTop: '1vh'}} 
-                        >
-                            Te estamos redirigiendo al inicio... {progress / 2 * 0.1}
-                        </Typography>
-                        <CircularProgress variant="determinate" value={progress} color='success' sx={{ marginTop: '4vh' }} size={50} />
+                    ? <div className={classes.root}>
+                        <Box sx={{ marginTop: '5vh' }}>
+                            <Box className={classes.checkContainer}>
+                                <img src={Check} alt="check mark" className={classes.check} />
+                                <Typography component='h2' variant='h4' className={classes.message1}>¡Compra exitosa!</Typography>
+                                <Typography className={classes.message2}>
+                                    Te estamos redirigiendo al inicio... {progress / 2 * 0.1}
+                                </Typography>
+                                <CircularProgress variant="determinate" value={progress} color='success' sx={{ marginTop: '4vh' }} size={50} />
                             </Box>
-                           
                         </Box>
-                        
                     </div>
                     : <div> Cargando...</div>
             }
